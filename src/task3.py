79e73d23613a243437b3473ab25ae7f2dab7c0f9
infinity0708 <XXC262@student.bham.ac.uk>
2024-12-02 20:05:49 +0000

    task3 finished

--- src/task3.py
+++ src/task3.py
@@ -1,9 +1,22 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
+import os
+
 
 dbs = {
     '_default': TinyDB('orders.json'),
     'preparation': TinyDB('preparation.json')
 }
+
+
+current_dir = os.path.dirname(__file__)
+order_db_path = os.path.join(current_dir, '../databases/order.json')
+
+order_db = TinyDB(order_db_path)
+User = Query()
+
+student_balance = 100
+delivery_fee = 10
+
 
 def insert(database, order):
     database.insert(order)
@@ -24,26 +37,15 @@ def move_orders_to_preparation():
             insert(preparation_db, order)
 
 
-import os
-from tinydb import TinyDB, Query
-
-current_dir = os.path.dirname(__file__)
-order_db_path = os.path.join(current_dir, '../databases/order.json')
-
-order_db = TinyDB(order_db_path)
-User = Query()
-
-student_balance = 100
-delivery_fee = 10
-
 # Process orders
 for order in order_db.all():
     users = order.get('users', [])
     
-    # Check if all users are ready
+    # Check if all users are readyß
     if all(user.get('isReady', False) for user in users):
         # 如果所有用户都 isReady，重置 isReset 状态为 False
         order_db.update({'isReset': False}, User.id == order.get('id'))
+
 
 def number_of_students(order_db):
     total_users = 0
@@ -51,3 +53,20 @@ def number_of_students(order_db):
         users = order.get('users', [])
         total_users += len(users)
     return total_users
+
+
+def calculate_total_cost(items, user_id, delivery_fee, total_students):
+    # Calculate personal items cost for the given user
+    personal_items_cost = sum(item['price'] * item['quantity'] 
+                              for item in items if item['user_id'] == user_id 
+                              and not item['isPublic'])
+    
+    # Calculate public items cost
+    public_items_cost = sum(item['price'] * item['quantity'] 
+                            for item in items if item['isPublic'])
+    
+    # Calculate total cost
+    total_cost = personal_items_cost 
+    + (delivery_fee + public_items_cost) / total_students
+
+    return total_cost
