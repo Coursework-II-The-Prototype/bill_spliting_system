cd551ca51459818ce1f939163ca6b5c8e90ff4ae
cxy-254 <cxy254@student.bham.ac.uk>
2024-12-06 23:15:38 +0000

    woopsy woopsy the comments

--- tests/task2.py
+++ tests/task2.py
@@ -39,181 +39,181 @@ def mock_db(monkeypatch):
     }
 
 
-# def test_supermarket_database():
-#     from utils import get_db
+def test_supermarket_database():
+    from utils import get_db
 
-#     supermarket_db = get_db("supermarket")
-#     all_products = supermarket_db.all()
-#     check_fields(
-#         all_products, ["item_id", "name", "price"], "supermarket database"
-#     )
-#     for p in all_products:
-#         type_check(p["item_id"], str, "`item_id`")
-#         type_check(p["name"], str, "`name`")
-#         type_check(p["price"], float, "`price`")
-
-
-# def test_get_order(mock_db):
-#     order_db = mock_db["order_db"]
-#     get_order = mock_db["get_order"]
-
-#     order_db.insert({"order_id": "-"})
-#     order_db.insert({"order_id": ":)"})
-#     assert not get_order("1")
-
-#     id = "myorder"
-#     order_db.insert({"order_id": id})
-#     order_db.insert({"order_id": ":("})
-#     assert get_order(id)["order_id"] == id
+    supermarket_db = get_db("supermarket")
+    all_products = supermarket_db.all()
+    check_fields(
+        all_products, ["item_id", "name", "price"], "supermarket database"
+    )
+    for p in all_products:
+        type_check(p["item_id"], str, "`item_id`")
+        type_check(p["name"], str, "`name`")
+        type_check(p["price"], float, "`price`")
 
 
-# def test_print(mock_db):
-#     order_db = mock_db["order_db"]
-#     get_order = mock_db["get_order"]
-#     get_personal_table = mock_db["get_personal_table"]
-#     get_public_table = mock_db["get_public_table"]
+def test_get_order(mock_db):
+    order_db = mock_db["order_db"]
+    get_order = mock_db["get_order"]
 
-#     order_db.insert(
-#         {
-#             "order_id": "1",
-#             "users": ["1", "2"],
-#             "items": [
-#                 {
-#                     "item_id": "1",
-#                     "quantity": 1,
-#                     "isPublic": False,
-#                     "user_id": "1",
-#                 },
-#                 {
-#                     "item_id": "3",
-#                     "quantity": 4,
-#                     "isPublic": True,
-#                     "user_id": "1",
-#                 },
-#                 {
-#                     "item_id": "2",
-#                     "quantity": 3,
-#                     "isPublic": False,
-#                     "user_id": "2",
-#                 },
-#                 {
-#                     "item_id": "4",
-#                     "quantity": 2,
-#                     "isPublic": True,
-#                     "user_id": "2",
-#                 },
-#             ],
-#             "isReset": False,
-#         }
-#     )
-#     order = get_order("1")
+    order_db.insert({"order_id": "-"})
+    order_db.insert({"order_id": ":)"})
+    assert not get_order("1")
 
-#     t1 = get_personal_table(order["items"], "1")
-#     assert len(t1) == 1
-#     assert t1[0][0] == "a"
-#     assert t1[0][1] == 4
-#     assert t1[0][2] == 1
-
-#     t2 = get_public_table(order["items"])
-#     assert len(t2) == 2
-#     assert t2[0][0] == "c"
-#     assert t2[0][1] == 3
-#     assert t2[0][2] == 4
-#     assert t2[0][3] == "1"
-
-#     assert t2[1][0] == "d"
-#     assert t2[1][1] == 1.5
-#     assert t2[1][2] == 2
-#     assert t2[1][3] == "2"
+    id = "myorder"
+    order_db.insert({"order_id": id})
+    order_db.insert({"order_id": ":("})
+    assert get_order(id)["order_id"] == id
 
 
-# def test_insert(mock_db, capsys):
-#     order_db = mock_db["order_db"]
-#     insert = mock_db["insert"]
+def test_print(mock_db):
+    order_db = mock_db["order_db"]
+    get_order = mock_db["get_order"]
+    get_personal_table = mock_db["get_personal_table"]
+    get_public_table = mock_db["get_public_table"]
 
-#     order_db.insert(
-#         {
-#             "order_id": "1",
-#             "users": [],
-#             "items": [],
-#             "isReset": False,
-#         }
-#     )
+    order_db.insert(
+        {
+            "order_id": "1",
+            "users": ["1", "2"],
+            "items": [
+                {
+                    "item_id": "1",
+                    "quantity": 1,
+                    "isPublic": False,
+                    "user_id": "1",
+                },
+                {
+                    "item_id": "3",
+                    "quantity": 4,
+                    "isPublic": True,
+                    "user_id": "1",
+                },
+                {
+                    "item_id": "2",
+                    "quantity": 3,
+                    "isPublic": False,
+                    "user_id": "2",
+                },
+                {
+                    "item_id": "4",
+                    "quantity": 2,
+                    "isPublic": True,
+                    "user_id": "2",
+                },
+            ],
+            "isReset": False,
+        }
+    )
+    order = get_order("1")
 
-#     inputs = [
-#         {"u": "user1", "i": "1", "a": "2", "p": "no"},
-#         {"u": "user3", "i": "2", "a": "4", "p": "yes"},
-#         {"u": "user2", "i": "3", "a": "6", "p": "no"},
-#     ]
-#     for index, inp in enumerate(inputs):
-#         with patch(
-#             "builtins.input", side_effect=[inp["i"], inp["a"], inp["p"]]
-#         ):
-#             assert insert(inp["u"], "1")
-#         capsys.readouterr()
+    t1 = get_personal_table(order["items"], "1")
+    assert len(t1) == 1
+    assert t1[0][0] == "a"
+    assert t1[0][1] == 4
+    assert t1[0][2] == 1
 
-#         order = order_db.all()[0]
-#         item = order["items"][-1]
-#         assert item["item_id"] == inp["i"]
-#         assert item["quantity"] == int(inp["a"])
-#         assert item["isPublic"] == (inp["p"] == "yes")
-#         assert item["user_id"] == inp["u"]
-#         if index < 1:
-#             assert (
-#                 order["isReset"] == False
-#             ), "isReset should be unchanged if order is not public"
-#         else:
-#             assert order["isReset"] == True
+    t2 = get_public_table(order["items"])
+    assert len(t2) == 2
+    assert t2[0][0] == "c"
+    assert t2[0][1] == 3
+    assert t2[0][2] == 4
+    assert t2[0][3] == "1"
 
-#     order = order_db.all()[0]
-#     items = order["items"]
-#     assert len(items) == 3
+    assert t2[1][0] == "d"
+    assert t2[1][1] == 1.5
+    assert t2[1][2] == 2
+    assert t2[1][3] == "2"
 
-#     check_fields(
-#         items, ["item_id", "quantity", "isPublic", "user_id"], "`items`"
-#     )
 
-#     for item in items:
-#         type_check(item["item_id"], str, "`item_id`")
-#         type_check(item["quantity"], int, "`quantity`")
-#         type_check(item["isPublic"], bool, "`isPublic`")
-#         type_check(item["user_id"], str, "`user_id`")
+def test_insert(mock_db, capsys):
+    order_db = mock_db["order_db"]
+    insert = mock_db["insert"]
 
-#     # check invalid id
-#     with patch("builtins.input", side_effect=["99", "1", "yes"]):
-#         assert not insert("user1", "1")
-#     capsys.readouterr()
+    order_db.insert(
+        {
+            "order_id": "1",
+            "users": [],
+            "items": [],
+            "isReset": False,
+        }
+    )
 
-#     # check same id increment
-#     order_db.truncate()
-#     order_db.insert(
-#         {
-#             "order_id": "1",
-#             "users": [],
-#             "items": [
-#                 {
-#                     "item_id": "1",
-#                     "quantity": 1,
-#                     "isPublic": False,
-#                     "user_id": "user1",
-#                 }
-#             ],
-#             "isReset": False,
-#         }
-#     )
-#     with patch("builtins.input", side_effect=["1", "2", "yes"]):
-#         assert insert("user1", "1")
-#     capsys.readouterr()
+    inputs = [
+        {"u": "user1", "i": "1", "a": "2", "p": "no"},
+        {"u": "user3", "i": "2", "a": "4", "p": "yes"},
+        {"u": "user2", "i": "3", "a": "6", "p": "no"},
+    ]
+    for index, inp in enumerate(inputs):
+        with patch(
+            "builtins.input", side_effect=[inp["i"], inp["a"], inp["p"]]
+        ):
+            assert insert(inp["u"], "1")
+        capsys.readouterr()
 
-#     assert len(order_db.all()[0]["items"]) == 2
-#     assert order_db.all()[0]["items"][1]["quantity"] == 2
+        order = order_db.all()[0]
+        item = order["items"][-1]
+        assert item["item_id"] == inp["i"]
+        assert item["quantity"] == int(inp["a"])
+        assert item["isPublic"] == (inp["p"] == "yes")
+        assert item["user_id"] == inp["u"]
+        if index < 1:
+            assert (
+                order["isReset"] == False
+            ), "isReset should be unchanged if order is not public"
+        else:
+            assert order["isReset"] == True
 
-#     with patch("builtins.input", side_effect=["1", "2", "no"]):
-#         assert insert("user1", "1")
-#     capsys.readouterr()
+    order = order_db.all()[0]
+    items = order["items"]
+    assert len(items) == 3
 
-#     assert len(order_db.all()[0]["items"]) == 2
-#     assert order_db.all()[0]["items"][0]["quantity"] == 3
+    check_fields(
+        items, ["item_id", "quantity", "isPublic", "user_id"], "`items`"
+    )
+
+    for item in items:
+        type_check(item["item_id"], str, "`item_id`")
+        type_check(item["quantity"], int, "`quantity`")
+        type_check(item["isPublic"], bool, "`isPublic`")
+        type_check(item["user_id"], str, "`user_id`")
+
+    # check invalid id
+    with patch("builtins.input", side_effect=["99", "1", "yes"]):
+        assert not insert("user1", "1")
+    capsys.readouterr()
+
+    # check same id increment
+    order_db.truncate()
+    order_db.insert(
+        {
+            "order_id": "1",
+            "users": [],
+            "items": [
+                {
+                    "item_id": "1",
+                    "quantity": 1,
+                    "isPublic": False,
+                    "user_id": "user1",
+                }
+            ],
+            "isReset": False,
+        }
+    )
+    with patch("builtins.input", side_effect=["1", "2", "yes"]):
+        assert insert("user1", "1")
+    capsys.readouterr()
+
+    assert len(order_db.all()[0]["items"]) == 2
+    assert order_db.all()[0]["items"][1]["quantity"] == 2
+
+    with patch("builtins.input", side_effect=["1", "2", "no"]):
+        assert insert("user1", "1")
+    capsys.readouterr()
+
+    assert len(order_db.all()[0]["items"]) == 2
+    assert order_db.all()[0]["items"][0]["quantity"] == 3
 
 
 def test_update(mock_db, capsys):
@@ -341,62 +341,62 @@ def test_update(mock_db, capsys):
     assert len(items) == 0
 
 
-# def test_reset_ready(mock_db):
-#     order_db = mock_db["order_db"]
-#     reset_ready = mock_db["reset_ready"]
+def test_reset_ready(mock_db):
+    order_db = mock_db["order_db"]
+    reset_ready = mock_db["reset_ready"]
 
-#     order_db.insert(
-#         {
-#             "order_id": "1",
-#             "users": [
-#                 {"user_id": "user1", "isReady": True},
-#             ],
-#             "items": [],
-#             "isReset": False,
-#         }
-#     )
-#     order_db.insert(
-#         {
-#             "order_id": "2",
-#             "users": [
-#                 {"user_id": "user2", "isReady": True},
-#                 {"user_id": "user3", "isReady": False},
-#                 {"user_id": "user4", "isReady": True},
-#             ],
-#             "items": [],
-#             "isReset": False,
-#         },
-#     )
-#     reset_ready("2")
-#     for u in order_db.all()[1]["users"]:
-#         assert u["isReady"] == False
-#     order_db.all()[0]["isReset"] == False
-#     order_db.all()[1]["isReset"] == True
+    order_db.insert(
+        {
+            "order_id": "1",
+            "users": [
+                {"user_id": "user1", "isReady": True},
+            ],
+            "items": [],
+            "isReset": False,
+        }
+    )
+    order_db.insert(
+        {
+            "order_id": "2",
+            "users": [
+                {"user_id": "user2", "isReady": True},
+                {"user_id": "user3", "isReady": False},
+                {"user_id": "user4", "isReady": True},
+            ],
+            "items": [],
+            "isReset": False,
+        },
+    )
+    reset_ready("2")
+    for u in order_db.all()[1]["users"]:
+        assert u["isReady"] == False
+    order_db.all()[0]["isReset"] == False
+    order_db.all()[1]["isReset"] == True
 
 
-# def test_setReady(mock_db):
-#     order_db = mock_db["order_db"]
-#     setReady = mock_db["setReady"]
+def test_setReady(mock_db):
+    order_db = mock_db["order_db"]
+    setReady = mock_db["setReady"]
 
-#     order_db.insert(
-#         {
-#             "order_id": "1",
-#             "users": [
-#                 {"user_id": "user1", "isReady": False},
-#                 {"user_id": "user2", "isReady": False},
-#             ],
-#             "items": [],
-#             "isReset": False,
-#         }
-#     )
+    order_db.insert(
+        {
+            "order_id": "1",
+            "users": [
+                {"user_id": "user1", "isReady": False},
+                {"user_id": "user2", "isReady": False},
+            ],
+            "items": [],
+            "isReset": False,
+        }
+    )
 
-#     with patch("builtins.input", side_effect=["no"]):
-#         setReady("user1", "1")
+    with patch("builtins.input", side_effect=["no"]):
+        setReady("user1", "1")
 
-#     assert order_db.all()[0]["users"][0]["isReady"] == False
+    assert order_db.all()[0]["users"][0]["isReady"] == False
 
-#     with patch("builtins.input", side_effect=["yes"]):
-#         setReady("user1", "1")
+    with patch("builtins.input", side_effect=["yes"]):
+        setReady("user1", "1")
 
-#     assert order_db.all()[0]["users"][0]["isReady"] == True
-#     assert order_db.all()[0]["users"][1]["isReady"] == False
+    assert order_db.all()[0]["users"][0]["isReady"] == True
+    assert order_db.all()[0]["users"][1]["isReady"] == False
