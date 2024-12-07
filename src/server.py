4a87f715a6d20a552df69da0ae08e65e2029ea6c
cxy-254 <cxy254@student.bham.ac.uk>
2024-12-07 16:58:56 +0000

    add premetheous metrics to server job

--- src/server.py
+++ src/server.py
@@ -1,9 +1,21 @@
 import task3
+import time
+from prometheus_client import start_http_server, Summary
 
 
+s = Summary("daily_job_seconds", "Time taken to finish to background job")
+
+
+@s.time()
 def main():
     task3.daily_job()
 
 
 if __name__ == "__main__":
+    start_http_server(8000)
     main()
+
+    # A loop to keep the script running
+    # So I can see the metrics at premetheous
+    while True:
+        time.sleep(1)
